4d877db77b60b9f2722ccab633694c99d964eb5c
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2022-09-19 15:32:51 +0200

    added AZ Queuetrigger

--- WebApplicationQueue/Program.cs
+++ WebApplicationQueue/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Services.AddHostedService<WeatherDataService>();
+//builder.Services.AddHostedService<WeatherDataService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
